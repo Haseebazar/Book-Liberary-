d5183e1778139921ba6044728e3c574cdc1012c8
haseebazar <l1f20bsse0428@ucp.edu.pk>
2024-05-08 12:24:00 +0500

    Your commit message

--- app/src/main/java/com/example/internship/MainActivity.java
+++ app/src/main/java/com/example/internship/MainActivity.java
@@ -123,6 +123,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
+            //change 4
         });
         builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
             @Override
